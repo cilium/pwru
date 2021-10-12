969d85592ac8d0081d67b057d17ce91be949582c
Martynas Pumputis <m@lambda.lt>
2021-10-12 16:25:28 +0200

    Add vendored dependencies
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

---
+++ vendor/github.com/cilium/cilium/pkg/byteorder/byteorder_bigendian.go
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Authors of Cilium
+
+//+build armbe arm64be mips mips64 ppc64
+
+package byteorder
+
+var Native binary.ByteOrder = binary.BigEndian
+
+func HostToNetwork16(u uint16) uint16 { return u }
+func HostToNetwork32(u uint32) uint32 { return u }
+func NetworkToHost16(u uint16) uint16 { return u }
+func NetworkToHost32(u uint32) uint32 { return u }
