1f4290802e37dc342ef0f4de2b7dbf03e8091d83
Tom Payne <tom@isovalent.com>
2021-10-19 12:34:38 +0000

    Fix printf verbs
    %w is for error wrapping and only available in fmt.Errorf.
    
    Signed-off-by: Tom Payne <tom@isovalent.com>

--- internal/pwru/config.go
+++ internal/pwru/config.go
@@ -20,7 +20,7 @@ func ConfigBPFMap(flags *Flags, cfgMap *ebpf.Map) {
 		key := uint32(CFG_FILTER_KEY_MARK)
 		val := uint32(*flags.FilterMark)
 		if err := cfgMap.Update(key, val, 0); err != nil {
-			log.Fatalf("Failed to set mark filter %d: %w", *flags.FilterMark, err)
+			log.Fatalf("Failed to set mark filter %d: %v", *flags.FilterMark, err)
 		}
 	}
 
@@ -38,7 +38,7 @@ func ConfigBPFMap(flags *Flags, cfgMap *ebpf.Map) {
 			key := uint32(CFG_FILTER_KEY_PROTO)
 			val := uint32(proto)
 			if err := cfgMap.Update(key, val, 0); err != nil {
-				log.Fatalf("Failed to set proto filter %s: %w", *flags.FilterProto, err)
+				log.Fatalf("Failed to set proto filter %s: %v", *flags.FilterProto, err)
 			}
 		}
 	}
@@ -48,7 +48,7 @@ func ConfigBPFMap(flags *Flags, cfgMap *ebpf.Map) {
 		val := byteorder.NetIPv4ToHost32(ip)
 		key := uint32(CFG_FILTER_KEY_DST_IP)
 		if err := cfgMap.Update(key, val, 0); err != nil {
-			log.Fatalf("Failed to set dst ip filter %s: %w", *flags.FilterDstIP, err)
+			log.Fatalf("Failed to set dst ip filter %s: %v", *flags.FilterDstIP, err)
 		}
 	}
 
@@ -57,32 +57,32 @@ func ConfigBPFMap(flags *Flags, cfgMap *ebpf.Map) {
 		val := byteorder.NetIPv4ToHost32(ip)
 		key := uint32(CFG_FILTER_KEY_SRC_IP)
 		if err := cfgMap.Update(key, val, 0); err != nil {
-			log.Fatalf("Failed to set src ip filter %s: %w", *flags.FilterSrcIP, err)
+			log.Fatalf("Failed to set src ip filter %s: %v", *flags.FilterSrcIP, err)
 		}
 	}
 
 	if *flags.FilterSrcPort != "" {
 		port, err := strconv.ParseUint(*flags.FilterSrcPort, 0, 16)
 		if err != nil {
-			log.Fatalf("Failed to parse src port %s: %w", *flags.FilterSrcPort, err)
+			log.Fatalf("Failed to parse src port %s: %v", *flags.FilterSrcPort, err)
 		}
 		key := uint32(CFG_FILTER_KEY_SRC_PORT)
 		val := uint32(byteorder.HostToNetwork16(uint16(port)))
 		if err := cfgMap.Update(key, val, 0); err != nil {
-			log.Fatalf("Failed to set src port filter %s: %w", *flags.FilterSrcPort, err)
+			log.Fatalf("Failed to set src port filter %s: %v", *flags.FilterSrcPort, err)
 		}
 	}
 
 	if *flags.FilterDstPort != "" {
 		port, err := strconv.ParseUint(*flags.FilterDstPort, 0, 16)
 		if err != nil {
-			log.Fatalf("Failed to parse dst port %s: %w", *flags.FilterDstPort, err)
+			log.Fatalf("Failed to parse dst port %s: %v", *flags.FilterDstPort, err)
 
 		}
 		key := uint32(CFG_FILTER_KEY_DST_PORT)
 		val := uint32(byteorder.HostToNetwork16(uint16(port)))
 		if err := cfgMap.Update(key, val, 0); err != nil {
-			log.Fatalf("Failed to set dst port filter %w: %w", *flags.FilterDstPort, err)
+			log.Fatalf("Failed to set dst port filter %s: %v", *flags.FilterDstPort, err)
 		}
 	}
 
@@ -90,7 +90,7 @@ func ConfigBPFMap(flags *Flags, cfgMap *ebpf.Map) {
 		key := uint32(CFG_OUTPUT_META)
 		val := uint32(1)
 		if err := cfgMap.Update(key, val, 0); err != nil {
-			log.Fatalf("Failed to set output meta: %w", err)
+			log.Fatalf("Failed to set output meta: %v", err)
 		}
 	}
 
@@ -98,7 +98,7 @@ func ConfigBPFMap(flags *Flags, cfgMap *ebpf.Map) {
 		key := uint32(CFG_OUTPUT_TUPLE)
 		val := uint32(1)
 		if err := cfgMap.Update(key, val, 0); err != nil {
-			log.Fatalf("Failed to set output tuple: %w", err)
+			log.Fatalf("Failed to set output tuple: %v", err)
 		}
 	}
 
@@ -106,7 +106,7 @@ func ConfigBPFMap(flags *Flags, cfgMap *ebpf.Map) {
 		key := uint32(CFG_OUTPUT_SKB)
 		val := uint32(1)
 		if err := cfgMap.Update(key, val, 0); err != nil {
-			log.Fatalf("Failed to set output skb: %w", err)
+			log.Fatalf("Failed to set output skb: %v", err)
 		}
 	}
 }
