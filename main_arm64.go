3ab3b9ef1615698c6b495b36306e989182101d71
Chen Yaqi <chendotjs@gmail.com>
2022-06-28 05:49:58 +0000

    Silence clang build warnings complaining address-of-packed-member
    Running either `make release` or `make local-release` will cause clang
    to emit warning messages of type `-Waddress-of-packed-member`.
    
    This commit silences the warnings by explicitly ignoring it.
    
    Signed-off-by: Chen Yaqi <chendotjs@gmail.com>

--- main_arm64.go
+++ main_arm64.go
@@ -2,7 +2,7 @@
 // Copyright (C) 2021 Authors of Cilium */
 
 //go:generate sh -c "echo Generating for arm64"
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbePWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -D__TARGET_ARCH_arm64 -I./bpf/headers
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbePWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D__TARGET_ARCH_arm64 -I./bpf/headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbePWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -D__TARGET_ARCH_arm64 -I./bpf/headers -Wno-address-of-packed-member
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbePWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D__TARGET_ARCH_arm64 -I./bpf/headers -Wno-address-of-packed-member
 
 package main
