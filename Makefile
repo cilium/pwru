cilium/pwru

commit 62d0dffbcc5dcdc550d9d5619af85ee1d812b7a2

main: pass -target to bpf2go
Use the -target flag to tell bpf2go which arch we are targeting. This
cuts down on compiling useless targets (big endian x86 anyone?)
bpf2go will automatically pass the appropriate __TARGET_ARCH_xxx.

Also use $GOARCH from as set by go generate to reduce the necessary
boilerplate.

build.go
@@ -1,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (C) 2021 Authors of Cilium */
+
+//go:generate sh -c "echo Generating for $GOARCH"
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $GOARCH -cc clang KProbePWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -I./bpf/headers -Wno-address-of-packed-member
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $GOARCH -cc clang KProbeMultiPWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -DHAS_KPROBE_MULTI -I./bpf/headers -Wno-address-of-packed-member
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $GOARCH -cc clang KProbePWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -I./bpf/headers -Wno-address-of-packed-member
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $GOARCH -cc clang KProbeMultiPWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D HAS_KPROBE_MULTI -I./bpf/headers -Wno-address-of-packed-member
+//go:generate go run ./tools/getgetter.go -struct ^(KProbePWRU|KProbeMultiPWRU|KProbePWRUWithoutOutputSKB|KProbeMultiPWRUWithoutOutputSKB)(Programs|Maps)$
+
+package main

main_amd64.go
@@ -1,11 +1,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-// Copyright (C) 2021 Authors of Cilium */
-
-//go:generate sh -c "echo Generating for amd64"
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbePWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -D__TARGET_ARCH_x86 -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbeMultiPWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -DHAS_KPROBE_MULTI -D__TARGET_ARCH_x86 -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbePWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D__TARGET_ARCH_x86 -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbeMultiPWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D HAS_KPROBE_MULTI -D__TARGET_ARCH_x86 -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run ./tools/getgetter.go -struct ^(KProbePWRU|KProbeMultiPWRU|KProbePWRUWithoutOutputSKB|KProbeMultiPWRUWithoutOutputSKB)(Programs|Maps)$
-
-package main

main_arm64.go
@@ -1,11 +1,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-// Copyright (C) 2021 Authors of Cilium */
-
-//go:generate sh -c "echo Generating for arm64"
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbePWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -D__TARGET_ARCH_arm64 -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbeMultiPWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -DHAS_KPROBE_MULTI -D__TARGET_ARCH_arm64 -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbePWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D__TARGET_ARCH_arm64 -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang KProbeMultiPWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D HAS_KPROBE_MULTI -D__TARGET_ARCH_arm64 -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run ./tools/getgetter.go -struct ^(KProbePWRU|KProbeMultiPWRU|KProbePWRUWithoutOutputSKB|KProbeMultiPWRUWithoutOutputSKB)(Programs|Maps)$
-
-package main

commit bcade6690de784ef0826eef2482db1317e7a783a

build: Rename arch variable
Otherwise, running "GOARCH=arm64 go generate build.go" results into
cross-compiling bpf2go for arm64 when running on a amd64 host.

Fixes: 62d0dffbc ("main: pass -target to bpf2go")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

Makefile
@@ -2,6 +2,7 @@ GO := go
 GO_BUILD = CGO_ENABLED=1 $(GO) build
 GO_GENERATE = $(GO) generate
 GO_TAGS ?=
+TARGET_GOARCH ?= amd64
 TARGET=pwru
 INSTALL = $(QUIET)install
 BINDIR ?= /usr/local/bin
@@ -10,7 +11,7 @@ VERSION=$(shell git describe --tags --always)
 TEST_TIMEOUT ?= 5s
 
 $(TARGET): libpcap/libpcap.a
-	$(GO_GENERATE)
+	TARGET_GOARCH=$(TARGET_GOARCH) $(GO_GENERATE)
 	$(GO_BUILD) $(if $(GO_TAGS),-tags $(GO_TAGS)) \
 		-ldflags "-w -s \
 		-X 'github.com/cilium/pwru/internal/pwru.Version=${VERSION}'"

build.go
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // Copyright (C) 2021 Authors of Cilium */
 
-//go:generate sh -c "echo Generating for $GOARCH"
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $GOARCH -cc clang KProbePWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $GOARCH -cc clang KProbeMultiPWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -DHAS_KPROBE_MULTI -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $GOARCH -cc clang KProbePWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -I./bpf/headers -Wno-address-of-packed-member
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $GOARCH -cc clang KProbeMultiPWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D HAS_KPROBE_MULTI -I./bpf/headers -Wno-address-of-packed-member
+//go:generate sh -c "echo Generating for $TARGET_GOARCH"
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET_GOARCH -cc clang KProbePWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -I./bpf/headers -Wno-address-of-packed-member
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET_GOARCH -cc clang KProbeMultiPWRU ./bpf/kprobe_pwru.c -- -DOUTPUT_SKB -DHAS_KPROBE_MULTI -I./bpf/headers -Wno-address-of-packed-member
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET_GOARCH -cc clang KProbePWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -I./bpf/headers -Wno-address-of-packed-member
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET_GOARCH -cc clang KProbeMultiPWRUWithoutOutputSKB ./bpf/kprobe_pwru.c -- -D HAS_KPROBE_MULTI -I./bpf/headers -Wno-address-of-packed-member
 //go:generate go run ./tools/getgetter.go -struct ^(KProbePWRU|KProbeMultiPWRU|KProbePWRUWithoutOutputSKB|KProbeMultiPWRUWithoutOutputSKB)(Programs|Maps)$
 
 package main