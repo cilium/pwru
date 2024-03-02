13ce768f74ae7045f8c3a27185497d1c712a6815
Hengqi Chen <hengqi.chen@gmail.com>
2024-03-02 12:07:11 +0800

    bpf: Add vmlinux.h for LoongArch
    The vmlinux.h is generated on Linux v6.7. This enables
    building kprobe_pwru on LoongArch.
    
    Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>

--- bpf/headers/vmlinux.h
+++ bpf/headers/vmlinux.h
@@ -2,6 +2,8 @@
 #include "vmlinux-x86.h"
 #elif defined(__TARGET_ARCH_arm64)
 #include "vmlinux-arm64.h"
+#elif defined(__TARGET_ARCH_loongarch)
+#include "vmlinux-loongarch.h"
 #else
 #error "Unknown architecture"
 #endif
