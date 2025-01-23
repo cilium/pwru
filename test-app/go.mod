1ca9aaad19ee67f46dd7b4eef740a57ee62ce72a
Martynas Pumputis <m@lambda.lt>
2025-01-23 16:34:20 +0100

    test: Add a test app which loads dummy TC/XDP BPF progs
    This is going to be used to test --filter-track-{xdp,tc}, and later on
    --filter-track-bpf-helpers.
    
    This commit adds the compiled BPF progs, to reduce the runtime
    dependency burden for the GH workflows (no need to install Clang).
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

---
+++ test-app/go.mod
@@ -0,0 +1,7 @@
+module github.com/cilium/pwru/test-app
+
+go 1.23.3
+
+require github.com/cilium/ebpf v0.17.1
+
+require golang.org/x/sys v0.26.0 // indirect
---
+++ test-app/main.go
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: Apache-2.0
+/* Copyright Authors of Cilium */
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 -cc clang bpf bpf.c -- -I../bpf/headers
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+)
+
+var (
+	ifaceName  = flag.String("iface", "", "iface to attach BPF prog")
+	attachType = flag.String("attach-type", "", "attach type ('xdp', 'tc')")
+)
+
+func main() {
+	flag.Parse()
+
+	if *attachType != "xdp" && *attachType != "tc" {
+		log.Fatalf("invalid attach type: %q", *attachType)
+	}
+
+	iface, err := net.InterfaceByName(*ifaceName)
+	if err != nil {
+		log.Fatalf("cannot find iface: %q: %s", *ifaceName, err)
+	}
+
+	objs := bpfObjects{}
+	if err := loadBpfObjects(&objs, nil); err != nil {
+		log.Fatalf("loading objects: %s", err)
+	}
+	defer objs.Close()
+
+	var l link.Link
+
+	switch *attachType {
+	case "xdp":
+		l, err = link.AttachXDP(link.XDPOptions{
+			Program:   objs.XdpDummyProg,
+			Interface: iface.Index,
+		})
+		if err != nil {
+			log.Fatalf("could not attach XDP program: %s", err)
+		}
+	case "tc":
+		l, err = link.AttachTCX(link.TCXOptions{
+			Program:   objs.TcDummyProg,
+			Attach:    ebpf.AttachTCXIngress,
+			Interface: iface.Index,
+		})
+	}
+
+	if err := l.Pin(fmt.Sprintf("/sys/fs/bpf/test-app-%s", *attachType)); err != nil {
+		log.Fatalf("failed to pin: %s", err)
+	}
+}
