d9b0d25d9c8668a68bf7da76d626b8ab84a3e530
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2025-04-21 14:40:53 +0200

    ci: switch to self-built AMI
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- infra/arm64-runner/setup.sh
+++ infra/arm64-runner/setup.sh
@@ -42,45 +42,12 @@ get_github_runners_token() {
   echo "$RUNNER_TOKEN"
 }
 
-# set up dependencies
-sudo apt-get update || true
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install -y qemu-kvm jq docker.io docker-buildx make \
-     clang cpp gcc zlib1g-dev libaio-dev libcap-dev libelf-dev \
-     liburing-dev net-tools netcat-traditional socat \
-     iptables software-properties-common netcat-openbsd iproute2
-
-VERSION_GO_CONTAINERREGISTRY=v0.19.1
-URL="https://github.com/google/go-containerregistry/releases/download/$VERSION_GO_CONTAINERREGISTRY/go-containerregistry_Linux_arm64.tar.gz"
-curl -fSL $URL | sudo tar -xz -C /usr/local/bin crane
-crane version
-
-# set up SSH for LVH
-sudo ufw allow OpenSSH
-sudo ufw --force enable
-sudo sed -i "s/.*PasswordAuthentication .*/PasswordAuthentication no/g" /etc/ssh/sshd_config
-sudo sed -i "s/.*PubkeyAuthentication .*/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
-sudo systemctl restart ssh
-
-# prepare multi-runners
-mkdir -p multi-runners && cd multi-runners
-VERSION_MULTI_RUNNERS=v1.2.0
-curl -L -o multi-runners.tar.gz https://github.com/vbem/multi-runners/archive/refs/tags/$VERSION_MULTI_RUNNERS.tar.gz
-tar xzf ./multi-runners.tar.gz --strip-components=1
-
 # get GH token for runners provisioning
 GITHUB_RUNNERS_TOKEN=$(get_github_runners_token)
 
-# create runners
-sudo tee .env <<EOF
-MR_RELEASE_URL='https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-linux-arm64-2.321.0.tar.gz'
-EOF
+cd /opt/multi-runners
+
 for (( i=1; i<=$GITHUB_RUNNERS_COUNT; i++ ))
 do
   ./mr.bash add --org $GITHUB_ORG --group $GITHUB_GROUP --labels $GITHUB_LABELS --user runner-$i --token $GITHUB_RUNNERS_TOKEN
 done
-
-# prepare directories for LVH
-mkdir -p /home/runners && chown :runners /home/runners && chmod 774 /home/runners
-
