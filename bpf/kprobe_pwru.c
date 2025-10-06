23e9e68d524c8c3be4acf01d6705d487379080af
Leon Hwang <leon.hwang@linux.dev>
2025-10-06 21:41:32 +0800

    bpf: Harden getting func ip
    Since commit torvalds/linux@7d1cd70d4b16ff (v6.13), the private stack of
    bpf programs was introduced for fentry.
    
    As a result, when the private stack is used for fentry, it cannot
    retrieve the function IP by unwinding `rfp`.
    
    To avoid the issue caused by the private stack, it is able to retrieve
    the function IP by unwinding trampoline's `rfp`, which is `ctx + 8`
    always for our case.
    
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- bpf/kprobe_pwru.c
+++ bpf/kprobe_pwru.c
@@ -630,7 +630,7 @@ cont:
 	return true;
 }
 
-static __always_inline u64 get_func_ip(void);
+static __always_inline u64 get_func_ip(void *ctx);
 
 static __always_inline u64
 get_addr(void *ctx, const bool is_kprobe, const bool has_get_func_ip) {
@@ -639,7 +639,7 @@ get_addr(void *ctx, const bool is_kprobe, const bool has_get_func_ip) {
 	if (has_get_func_ip) {
 		ip = bpf_get_func_ip(ctx); /* endbr has been handled in helper. */
 	} else {
-		ip = is_kprobe ? PT_REGS_IP((struct pt_regs *) ctx) : get_func_ip();
+		ip = is_kprobe ? PT_REGS_IP((struct pt_regs *) ctx) : get_func_ip(ctx);
 #ifdef bpf_target_x86
 		ip -= ENDBR_INSN_SIZE;
 		ip -= is_kprobe; /* -1 always on x86 if kprobe. */
@@ -770,12 +770,11 @@ detect_tramp_fp(void) {
 #endif
 
 static __always_inline u64
-get_func_ip(void) {
+get_func_ip(void *ctx) {
 	u64 fp_tramp, ip;
 
 #if defined(bpf_target_x86)
 	static const int ip_offset = 5/* sizeof callq insn */;
-	u64 fp;
 #elif defined(bpf_target_arm64)
 	/* Ref: commit b2ad54e1533e ("bpf, arm64: Implement bpf_arch_text_poke() for arm64") */
 	static const int ip_offset = 12/* sizeof 3 insns */;
@@ -793,6 +792,8 @@ get_func_ip(void) {
 	 * | rip | IP of tracee
 	 * | rbp | FP of tracee's caller
 	 * +-----+ FP of trampoline
+	 * | skb |
+	 * +-----+ ctx of fentry
 	 * | ... |
 	 * | rip | IP of trampoline
 	 * | rbp | FP of trampoline
@@ -823,8 +824,7 @@ get_func_ip(void) {
 	 */
 
 #if defined(bpf_target_x86)
-	fp = get_tracing_fp(); /* FP of current prog */
-	bpf_probe_read_kernel(&fp_tramp, sizeof(fp_tramp), (void *)fp); /* FP of trampoline */
+	fp_tramp = (u64) ctx + 8; /* FP of trampoline */
 #elif defined(bpf_target_arm64)
 	fp_tramp = detect_tramp_fp(); /* FP of trampoline */
 #endif
